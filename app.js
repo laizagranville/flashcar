3880437a556ab9badf9eafae68e99e184e17b443
laíza <164918287+laizagranville@users.noreply.github.com>
2024-11-18 13:43:43 -0300

    uk

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ function cricartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = ('cartao')
+
+    cartao.innerHTML =  <div class="cartao__conteudo">
+    <h3>progamação</h3>
+      <div class="cartao__conteudo__pergunta">
+       <p>python?</p>
+      </div>
+    <div class="cartao__conteudo__pergunta">
+ <p> O javaScript é uma linguaguem de progamação.</p>
+</div>
+</div>
 }
